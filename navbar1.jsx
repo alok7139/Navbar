8eb9487c422351fc796f89e9d27d2d4bd1417851
alok7139 <alokgarg7139@gmail.com>
2025-10-15 11:22:09 +0530

    commit

--- navbar1.jsx
+++ navbar1.jsx
@@ -1,51 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css"; // custom styling
+import "./LandingNavbar.css";
+import logoImg from "./logo.png"; // Replace with your logo path
 
-const Navbar = () => {
+const LandingNavbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
-      <div className="container">
-        {/* Logo */}
-        <Link className="navbar-brand fw-bold text-primary" to="/">
-          MyLogo
+    <nav className="lnb-navbar">
+      <div className="lnb-container">
+        <Link to="/" className="lnb-logo">
+          <img src={logoImg} alt="Brand Logo" className="lnb-logo-img" />
         </Link>
-
-        {/* Toggler for mobile */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+        <div
+          className={`lnb-hamburger ${menuOpen ? "lnb-hamburger-active" : ""}`}
+          onClick={toggleMenu}
         >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        {/* Links */}
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">About Us</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contact</Link>
-            </li>
-          </ul>
-
-          {/* Login Button */}
-          <Link to="/login" className="btn btn-primary ms-lg-3">
-            Login
-          </Link>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
+        <ul className={`lnb-menu ${menuOpen ? "lnb-menu-active" : ""}`}>
+          <li className="lnb-item">
+            <Link to="/" className="lnb-link">
+              Home
+            </Link>
+          </li>
+          <li className="lnb-item">
+            <Link to="/about" className="lnb-link">
+              About
+            </Link>
+          </li>
+          <li className="lnb-item">
+            <Link to="/services" className="lnb-link">
+              Services
+            </Link>
+          </li>
+          <li className="lnb-item">
+            <Link to="/contact" className="lnb-link">
+              Contact
+            </Link>
+          </li>
+          <li className="lnb-item">
+            <Link to="/signup" className="lnb-button">
+              Sign Up
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
 };
 
-export default Navbar;
+export default LandingNavbar;
+
